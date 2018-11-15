37ab1bdf6d08909fb2fe9dec2ae084dd9fcf128d
Ethan Lu <elu@nevada.unr.edu>
2018-11-14 23:36:19 -0500

    Basic stuff

---
+++ qtdemo.py
@@ -0,0 +1,13 @@
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
+def main():
+    app = QApplication([])
+    window = QWidget()
+    layout = QVBoxLayout()
+    layout.addWidget(QPushButton('Top'))
+    layout.addWidget(QPushButton('Bottom'))
+    window.setLayout(layout)
+    window.show()
+    app.exec_()
+
+if __name__ == "__main__":
+    main()
