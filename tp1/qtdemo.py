06062a3246e9113eacf5a1ddf1291f4a6ff14d77
Ethan Lu <elu@nevada.unr.edu>
2018-11-16 02:04:16 -0500

    update qt demo

--- tp1/qtdemo.py
+++ tp1/qtdemo.py
@@ -1,4 +1,5 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QGridLayout, QComboBox, QProgressBar
+from PyQt5.QtWidgets import QApplication, QWidget, QLabel, QPushButton, QGridLayout, QComboBox, QProgressBar
+from PyQt5.QtGui import QIcon, QPixmap
 import sys
 
 PATH_TO_DOLPHIN = "Dolphin.exe"
@@ -34,27 +35,39 @@ class MainWindow(QWidget):
         # Basic Button Functionality
         self.button =  QPushButton(str(self.n), self)
         self.button.clicked.connect(self.buttonClicked)
-        layoutGrid.addWidget(self.button, 0,0)
+        layoutGrid.addWidget(self.button, 1, 1)
         
-        # Dropdown 
-        self.dropDownMenu = QComboBox(self)
-        entries = ["a", "b", "c"]
-        for i in range(len(entries)):
-            self.dropDownMenu.insertItem(i, entries[i])
-        layoutGrid.addWidget(self.dropDownMenu, 0, 1)
-
         # Progress Bar
         self.progressBar = QProgressBar(self)
         self.progressBar.setMinimum(0)
         self.progressBar.setMaximum(100)
-        layoutGrid.addWidget(self.progressBar, 0, 2)
+        layoutGrid.addWidget(self.progressBar, 1, 2)
+
+        # Dropdown 
+        self.dropDownMenu = QComboBox(self)
+        self.entries = ["qtdemo/screenshot1.png", "qtdemo/screenshot2.png"]
+        for i in range(len(self.entries)):
+            self.dropDownMenu.insertItem(i, self.entries[i])
+        self.dropDownMenu.currentIndexChanged.connect(self.updateImage)
+        layoutGrid.addWidget(self.dropDownMenu, 1, 0)
+
+        # Images
+        self.img = QLabel(self)
+        pixmap = QPixmap(self.entries[0])
+        self.img.setPixmap(pixmap)
+        layoutGrid.addWidget(self.img, 0, 0)
+
+
 
         # Show the window 
         self.show()
 
-
     def buttonClicked(self, event):
         self.n = self.n + 1
+
+    def updateImage(self, newIndex):
+        pixmap = QPixmap(self.entries[newIndex])
+        self.img.setPixmap(pixmap)
 
 def main():
     app = QApplication([])
