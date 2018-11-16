bcdb1f6c4a0c2c5f70354855197c4c39609d0cc6
Ethan Lu <elu@nevada.unr.edu>
2018-11-16 00:25:53 -0500

    qtdemo update

--- tp1/qtdemo.py
+++ tp1/qtdemo.py
@@ -1,17 +1,57 @@
-from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QVBoxLayout
+from PyQt5.QtWidgets import QApplication, QLabel, QWidget, QPushButton, QGridLayout, QComboBox
+import sys
 
 PATH_TO_DOLPHIN = "Dolphin.exe"
 PATH_TO_GAME = ""
 
+class MainWindow(QWidget):
+    def __init__(self):
+        super().__init__()
+        # We need to instantiate a "private" variable and define getters 
+        # and setters on it for QT databinding to work properly.
+        self._n = 0
+        self.initUI()
+
+    @property
+    def n(self):
+        return self._n
+
+    @n.setter
+    def n(self, value):
+        self.button.setText(str(value))
+        self._n = value
+
+
+    
+
+
+    def initUI(self):
+        self.setGeometry(600,600,600,600)
+        layoutGrid = QGridLayout()
+        # Basic Button Functionality
+        self.button =  QPushButton(str(self.n), self)
+        self.button.clicked.connect(self.buttonClicked)
+        layoutGrid.addWidget(self.button, 4,4)
+        
+        # Dropdown 
+        self.dropDownMenu = QComboBox(self)
+        entries = ["a", "b", "c"]
+        for i in range(len(entries)):
+            self.dropDownMenu.insertItem(i, entries[i])
+        layoutGrid.addWidget(self.dropDownMenu, 3, 4)
+
+        # Progress Bar
+        
+        self.show()
+
+
+    def buttonClicked(self, event):
+        self.n = self.n + 1
+
 def main():
     app = QApplication([])
-    window = QWidget()
-    layout = QVBoxLayout()
-    layout.addWidget(QPushButton('Top'))
-    layout.addWidget(QPushButton('Bottom'))
-    window.setLayout(layout)
-    window.show()
-    app.exec_()
+    window = MainWindow()
+    sys.exit(app.exec_())
 
 if __name__ == "__main__":
     main()
