6f5af9f8ef94c0f33de0ae3fe4f6a8d46425806c
Ethan Lu <elu@nevada.unr.edu>
2018-11-27 13:54:38 -0500

    TP2 Update

--- tp2/update.bat
+++ tp2/update.bat
@@ -1,5 +1,5 @@
 rm tp2.zip
 git add .
-git commit -m "Update"
+git commit -m "TP2 Update"
 git push
-7z a -tzip "tp2.zip" "proposal.pdf" "storyboard.jpg" "src/" "styles/" 
+7z a -tzip "tp2.zip" "proposal.pdf" "storyboard.jpg" "src/*.py"  "src/icon.png" "styles/*/style.jpg" "style/*/preview.jpg" "styles/*/cfg.txt" "src/dump/tex1_48x24_f2707ea31b3c1aa8_14.dds" 
