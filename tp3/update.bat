76c1d510088a7db5a2f64aaa595c5a9e3e639dcc
Ethan Lu <elu@nevada.unr.edu>
2018-12-06 11:10:37 -0500

    TP3 Update

--- tp3/update.bat
+++ tp3/update.bat
@@ -2,4 +2,4 @@ rm tp3.zip
 git add .
 git commit -m "TP3 Update"
 git push
-7z a -tzip "tp3.zip" "proposal.pdf" "storyboard.jpg" "src/*.py"  "src/icon.png" "styles/*/style.jpg" "styles/*/preview.jpg" "styles/*/cfg.txt" "src/dump/" "README.txt" 
+7z a -tzip "tp3.zip" "proposal.pdf" "storyboard.jpg" "src/*.py"  "src/icon.png" "styles/*/style.jpg" "styles/*/preview.jpg" "styles/*/cfg.txt" "src/dump/" "README.txt" "styles/images" 
